8c2089e178afb42ad9c7000bf0fc860223dc62ed
ben straumann <benstraum@gmail.com>
2020-06-17 16:09:53 -0500

    made framework for sagas alongside implementing middleware

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
+//middleware
+import logger from 'redux-logger';
+import {takeEvery, put} from 'redux-saga/effects'
+import createSagaMiddleware from 'redux-saga';
+
 import App from './App';
+
+function* rootSaga() {
+ 
+}
+
+const sagaMiddleware = createSagaMiddleware();
 
 // this startingPlantArray should eventually be removed
 const startingPlantArray = [
@@ -23,6 +34,8 @@ const plantList = (state = startingPlantArray, action) => {
 
 const store = createStore(
   combineReducers({ plantList }),
+  applyMiddleware(sagaMiddleware, logger),
 );
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
